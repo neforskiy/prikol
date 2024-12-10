854332593f5f77dd6fa999b4eeec64cabef31548
Кальченко Тимофей <hackerbro228007@mail.ru>
2024-12-10 20:30:00 +0300

    f

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ const Router = require('express');
 const router = new Router();
 const testRouter = require('./testRouter');
 
+// Обработчик для корневого маршрута API
+router.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the API!' });
+});
+
 router.use('/test', testRouter); // Этот маршрут будет доступен по /api/test
 
 module.exports = router;
